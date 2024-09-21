738e23c6e3e31af2defef24f37a33001795603c9
UtopicRay <rayrm@gmail.com>
2024-09-21 19:12:31 -0400

    Update Portfolio

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import "./Nabvar.css";
 function Navbar() {
   const [nav, setNav] = useState(false);
   function handleNav() {
@@ -7,11 +8,10 @@ function Navbar() {
   }
   return (
     <>
-      <header className="fixed top-0 p-5 w-full flex flex-wrap bg-white bg-opacity-20 backdrop-blur-2xl text-[#F9E8C9] md:justify-center items-center z-10 justify-end">
-        <ul className="md:flex gap-2 [&&>li]:p-5 hidden text-white font-bold text-xl">
+      <header className="header top-0 p-5 w-full flex flex-wrap text-[#F9E8C9] md:justify-center items-center z-10 justify-end">
+        <ul className="md:flex gap-2 [&&>li]:px-5 hidden text-white font-bold text-xl">
           <li>
             <a href="#about" className="list-item text-sm">
-              {" "}
               Sobre Mí
             </a>
           </li>
@@ -27,7 +27,7 @@ function Navbar() {
           </li>
         </ul>
         <button className="md:block hidden bg-[#2A629A] px-4 py-2 rounded-lg text-gray-200 hover:bg-slate-700 duration-300">
-          <a href="contact" className="text-white font-bold">
+          <a href="#contact" className="text-white font-bold">
             Contact
           </a>
         </button>
@@ -37,15 +37,14 @@ function Navbar() {
         <ul
           className={
             nav
-              ? `text-gray-300 text-left text-xl fixed left-0 top-0 w-[60%] bg-black h-[100vh] border-r border-r-gray-900 ease-in-out duration-500 z-50 backdrop-blur-lg bg-opacity-25`
+              ? ` text-gray-300 text-left text-xl fixed left-0 top-0 w-[60%] [&&>li]:my-8 bg-black h-[100vh] border-r border-r-gray-900 ease-in-out duration-500 z-50 backdrop-blur-lg bg-opacity-25`
               : " text-left fixed left-[-100%] ease-in-out duration-500 top-0 h-full w-[60%]"
           }
         >
-          <h1 className="ml-2 text-3xl font-bold">Ray</h1>
           <li>
             <a
               href="#about"
-              className="list-item p-5"
+              className="list-item p-5 mt-10"
               onClick={() => setNav(false)}
             >
               {" "}
@@ -70,8 +69,8 @@ function Navbar() {
               Proyectos
             </a>
           </li>
-          <li>
-            <button className="bg-[#2A629A] px-4 py-2 rounded-lg text-gray-200 hover:bg-slate-700 duration-300">
+          <li className="p-5">
+            <button className="bg-[#2A629A] px-4 py-2 rounded-lg text-gray-200 hover:bg-slate-700 duration-300 ">
               <a
                 href="#contact"
                 className="text-white font-bold"
