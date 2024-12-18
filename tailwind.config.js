1d5ace5d9b5bb0957f31306ba85b3abf052baf7c
UtopicRay <rayrm@gmail.com>
2024-12-18 11:46:10 -0500

    update package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import animate  from 'tailwindcss-animated'
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-animated'),],
+  plugins: [animate],
 };
